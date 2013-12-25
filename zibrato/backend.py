c3666d3c58d057fcf15fb8f41701ad521fcb9c32
Andy Davidoff <andy+github@disruptek.com>
2013-12-25 00:04:42 -0500

    only pep8 and very light idiomatic changes

--- zibrato/backend.py
+++ zibrato/backend.py
@@ -1,91 +1,99 @@
 import zmq
-import collections
+from collections import namedtuple
 import argparse
 from fuzzywuzzy import process
-from time import sleep
 
 __version_info__ = (0, 1, 8)
-__version__ = ".".join(map(lambda x: str(x), __version_info__))
+__version__ = ".".join([str(x) for x in __version_info__])
 
-TYPES_OF_METRICS = [
-      'counters', 'timers', 'gauges'
-    ]
+TYPES_OF_METRICS = ['counters', 'timers', 'gauges']
 
-Measurement = collections.namedtuple(
-    'Measurement',
-    'name, value, source, measure_time'
-  )
+Measurement = namedtuple('Measurement',
+                         ('name', 'value', 'source', 'measure_time'))
+
 
 class Backend(object):
-  def __init__(self, **kwargs):
-    for key, value in kwargs.items():
-      setattr(self, key, value)
-    context = kwargs.get('context') or zmq.Context()
-    host = kwargs.get('host') or '127.0.0.1'
-    port = kwargs.get('port') or 5551
-    self.context = context
-    self.socket = self.context.socket(zmq.SUB)
-    self.socket.connect('tcp://%s:%d' % (host, int(port)))
-    self.queue = {}
-  def subscribe(self, sub):
-    self.socket.setsockopt(zmq.SUBSCRIBE, sub)
-  def receive_one(self):
-    self.socket.RCVTIMEO = 100
-    ret = self.socket.recv()
-    self.socket.RCVTIMEO = 0
-    return ret
-  def receive(self):
-    return self.socket.recv()
-  def parse(self, message):
-    (level, mtype, name, value, timestamp, source) = message.split('|')
-    (mtype, confidence) = process.extractOne(
-        mtype, TYPES_OF_METRICS)
-    return ( mtype,
-             Measurement._make((name, float(value), source, timestamp)))
-  def connect(self):
-    pass
-  def post(self, message):
-    mtype, measurement = self.parse(message)
-    try:
-      self.queue[mtype].append(measurement)
-    except KeyError:
-      self.queue[mtype] = [measurement]
-  def flush(self, message):
-    self.queue = {}
-  def close(self):
-    self.socket.close()
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        # ugly preservation of `default if kwarg is None else kwarg` semantic
+        self.host = getattr(self, 'host', None) or '127.0.0.1'
+        self.port = getattr(self, 'port', None) or 5551
+        self.context = getattr(self, 'context', None) or zmq.Context()
+        self.socket = self.context.socket(zmq.SUB)
+        self.socket.connect('tcp://%s:%d' % (self.host, int(self.port)))
+        self.queue = {}
+
+    def subscribe(self, sub):
+        self.socket.setsockopt(zmq.SUBSCRIBE, sub)
+
+    def receive_one(self):
+        self.socket.RCVTIMEO = 100
+        ret = self.socket.recv()
+        self.socket.RCVTIMEO = 0
+        return ret
+
+    def receive(self):
+        return self.socket.recv()
+
+    def parse(self, message):
+        level, mtype, name, value, timestamp, source = message.split('|')
+        mtype, confidence = process.extractOne(mtype, TYPES_OF_METRICS)
+        return mtype, Measurement(name, float(value), source, timestamp)
+
+    def connect(self):
+        pass
+
+    def post(self, message):
+        mtype, measurement = self.parse(message)
+        if mtype not in self.queue:
+            self.queue[mtype] = []
+        self.queue[mtype].append(measurement)
+
+    def flush(self, message):
+        self.queue = {}
+
+    def close(self):
+        self.socket.close()
+
 
 class Broker(object):
-  def __init__(self, **kwargs):
-    self.host = kwargs.get('host') or '127.0.0.1'
-    self.port = int(kwargs.get('port')) or 5550
-    self.context = kwargs.get('context') or zmq.Context.instance()
-    self.frontend = self.context.socket(zmq.SUB)
-    self.frontend.bind('tcp://%s:%d' % (self.host, self.port))
-    self.frontend.setsockopt(zmq.SUBSCRIBE, '')
-    self.backend = self.context.socket(zmq.PUB)
-    self.backend.bind('tcp://%s:%d' % (self.host, self.port+1))
-    self.backend.setsockopt(zmq.LINGER, 0)
-  def main(self):
-    try:
-      zmq.device(zmq.FORWARDER, self.frontend, self.backend)
-    except zmq.ZMQError as e:
-      if e.errno == zmq.ETERM:
-        self.frontend.close()
-        self.backend.close()
-      else:
-        raise
+
+    def __init__(self, **kwargs):
+        self.host = kwargs.get('host') or '127.0.0.1'
+        self.port = int(kwargs.get('port')) or 5550
+        self.context = kwargs.get('context') or zmq.Context.instance()
+        self.frontend = self.context.socket(zmq.SUB)
+        self.frontend.bind('tcp://%s:%d' % (self.host, self.port))
+        self.frontend.setsockopt(zmq.SUBSCRIBE, '')
+        self.backend = self.context.socket(zmq.PUB)
+        self.backend.bind('tcp://%s:%d' % (self.host, self.port + 1))
+        self.backend.setsockopt(zmq.LINGER, 0)
+
+    def main(self):
+        try:
+            zmq.device(zmq.FORWARDER, self.frontend, self.backend)
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                self.frontend.close()
+                self.backend.close()
+            else:
+                raise
+
 
 def main():
-  parser = argparse.ArgumentParser(
-    description = 'Backend manager for Zibrato')
-  parser.add_argument('--host', default = '127.0.0.1',
-      help='TCP address first or FQDN of listener (default 127.0.0.1)')
-  parser.add_argument('--port', default = '5550',
-      help='Lower port of pair (default 5550)')
-  args = parser.parse_args()
-  b = Broker(host = args.host, port = args.port)
-  b.main()
+    parser = argparse.ArgumentParser(
+        description='Backend manager for Zibrato',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--host', default='127.0.0.1',
+                        help='TCP address first or FQDN of listener')
+    parser.add_argument('--port', type=int, default=5550,
+                        help='Lower port of pair')
+    args = parser.parse_args()
+    b = Broker(host=args.host, port=args.port)
+    b.main()
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- zibrato/librato.py
+++ zibrato/librato.py
@@ -1,115 +1,114 @@
-from backend import Backend
-from fuzzywuzzy import process
 from time import time
-import requests
-import simplejson as json
 import re
-import collections
+from collections import namedtuple
 import argparse
-import threading
+import simplejson as json
+import requests
+from fuzzywuzzy import process
+from .backend import Backend
 
 __version_info__ = (0, 1, 8)
-__version__ = ".".join(map(lambda x: str(x), __version_info__))
+__version__ = ".".join([str(x) for x in __version_info__])
 
 USER_AGENT = 'zibrato/%s' % __version__
 API = 'https://metrics-api.librato.com/v1/metrics'
-TYPES_OF_METRICS = [ 'counters', 'gauges' ]
+TYPES_OF_METRICS = ['counters', 'gauges']
 
-Measurement = collections.namedtuple(
-    'Measurement',
-    'name, value, source, measure_time'
-  )
+Measurement = namedtuple('Measurement',
+                         ('name', 'value', 'source', 'measure_time'))
+
 
 class Librato(Backend):
-  def connect(self):
-    r = requests.head(
-        API,
-        auth = (self.username, self.apitoken),
-        headers = {'User-Agent': USER_AGENT })
-    return r.status_code
-  def parse(self, message):
-    print message
-    (level, mtype, name, value, timestamp, source) = message.split('|')
-    if mtype == 'Timer': mtype = 'gauges'
-    (mtype, confidence) = process.extractOne(
-        mtype, TYPES_OF_METRICS)
-    source = re.sub(r'[^A-Za-z0-9.:-_]', ':', source)
-    return ( mtype,
-             Measurement._make((name, float(value), source, timestamp)))
-  def rollup_counters(self):
-    if not self.queue.has_key('counters'):
-      return
-    counter = {}
-    timestamp = str(int(float(time())))
-    for item in self.queue['counters']:
-      try:
-        counter[item.source + '|' + item.name] += item.value
-      except KeyError:
-        counter[item.source + '|' + item.name] = item.value
-    del self.queue['counters']
-    for (k, v) in counter.iteritems():
-      (source, name) = k.split('|')
-      self.post(
-          '|'.join(('', 'gauges', name, str(v), timestamp, source)))
-  def flush(self):
-    if self.queue:
-      self.rollup_counters()
-      auth = (self.username, self.apitoken)
-      data = json.dumps(self.queue)
-      headers = {
-          'content-type': 'application/json',
-          'User-Agent': USER_AGENT
-          }
-      r = requests.post(
-          API,
-          auth = auth,
-          data = data,
-          headers = headers)
-      self.queue = {}
-      if r.status_code > 200:
-        raise IOError(r.text + ' ' + data)
-      else:
+
+    def connect(self):
+        r = requests.head(
+            API,
+            auth=(self.username, self.apitoken),
+            headers = {'User-Agent': USER_AGENT})
         return r.status_code
 
+    def parse(self, message):
+        print message
+        level, mtype, name, value, timestamp, source = message.split('|')
+        if mtype == 'Timer':
+            mtype = 'gauges'
+        mtype, confidence = process.extractOne(mtype, TYPES_OF_METRICS)
+        source = re.sub(r'[^A-Za-z0-9.:-_]', ':', source)
+        return mtype, Measurement(name, float(value), source, timestamp)
+
+    def rollup_counters(self):
+        if not 'counters' in self.queue:
+            return
+        counter = {}
+        timestamp = str(int(time()))
+        for item in self.queue['counters']:
+            name = item.source + '|' + item.name
+            counter[name] = counter.get(name, 0) + item.value
+        del self.queue['counters']
+        for k, v in counter.iteritems():
+            source, name = k.split('|')
+            self.post('|'.join(('', 'gauges', name, str(v),
+                                timestamp, source)))
+
+    def flush(self):
+        if not self.queue:
+            return
+        self.rollup_counters()
+        auth = (self.username, self.apitoken)
+        data = json.dumps(self.queue)
+        headers = {
+            'content-type': 'application/json',
+            'User-Agent': USER_AGENT,
+        }
+        r = requests.post(
+            API,
+            auth=auth,
+            data=data,
+            headers=headers)
+        self.queue = {}
+        if r.status_code > 200:
+            raise IOError(r.text + ' ' + data)
+        return r.status_code
+
+
 def main():
-  parser = argparse.ArgumentParser(
-    description = 'Librato backend worker for Zibrato')
-  parser.add_argument('--host',
-      help = 'Zibrato backend FQDN or IP',
-      default = '127.0.0.1')
-  parser.add_argument('--port',
-      help = 'Zibrato backend port',
-      default = '5551')
-  parser.add_argument('--username',
-      help = 'Librato username')
-  parser.add_argument('--apitoken',
-      help = 'Librato API token')
-  parser.add_argument('--levels',
-      help = 'Subscribe to which levels')
-  parser.add_argument('--flush',
-      help = 'Flush time in seconds')
-  args = parser.parse_args()
-  l = Librato(
-      host = args.host,
-      port = args.port,
-      username = args.username,
-      apitoken = args.apitoken)
-  for level in args.levels.split(','):
-    l.subscribe(level.strip())
-  count_measurements = 0
-  count_flushes = 0
-  start = int(time())
-  while True:
-    try:
-      l.post(l.receive())
-      if int(time()) >= start + int(args.flush):
-        l.flush()
-        count_flushes += 1
-        start = int(time())
-    except KeyboardInterrupt:
-      raise KeyboardInterrupt('%d measurements / %d flushes' %
-          (count_measurements, count_flushes))
-    count_measurements += 1
+    parser = argparse.ArgumentParser(
+        description='Librato backend worker for Zibrato',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--host', default='127.0.0.1',
+                        help='Zibrato backend FQDN or IP')
+    parser.add_argument('--port', type=int, default=5551,
+                        help='Zibrato backend port')
+    parser.add_argument('--username',
+                        help='Librato username')
+    parser.add_argument('--apitoken',
+                        help='Librato API token')
+    parser.add_argument('--levels',
+                        help='Subscribe to which levels')
+    parser.add_argument('--flush', type=float,
+                        help='Flush time in (float) seconds')
+    args = parser.parse_args()
+    l = Librato(
+        host=args.host,
+        port=args.port,
+        username=args.username,
+        apitoken=args.apitoken)
+    for level in args.levels.split(','):
+        l.subscribe(level.strip())
+    count_measurements = 0
+    count_flushes = 0
+    start = time()
+    while True:
+        try:
+            l.post(l.receive())
+            if time() >= start + args.flush:
+                l.flush()
+                count_flushes += 1
+                start = time()
+        except KeyboardInterrupt:
+            raise KeyboardInterrupt('%d measurements / %d flushes' %
+                                    (count_measurements, count_flushes))
+        count_measurements += 1
 
 if __name__ == "__main__":
-  main()
+    main()
